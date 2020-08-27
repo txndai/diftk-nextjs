7237a35075ebbb62b99e1afc3f4699d0384762c5
GT Fari <guideg6@gmail.com>
2020-08-27 22:26:23 +0200

    graphcms intergrated

--- pages/magazine.js
+++ pages/magazine.js
@@ -1,51 +1,86 @@
 import Layout from "../components/layout";
+import { GraphQLClient } from "graphql-request";
+import Link from "next/link";
 
-function Magazine({ og }) {
-  return (
+const graphcms = new GraphQLClient(process.env.GRAPHQL_URL_ENDPOINT);
+
+export async function getStaticProps() {
+    const { blogPosts } = await graphcms.request(
+      `
+      query MyQuery() {
+        blogPosts {
+          id
+          title
+          summary
+          slug
+          image {
+            url
+            id
+          }
+          date
+          author {
+            name
+          }
+        }
+      }
+    `
+    );
+  
+    return {
+      props: {
+        blogPosts,
+      },
+    };
+}
+
+
+function Magazine({ og, blogPosts }) {
+    console.log(blogPosts)
+
+    return (
     <Layout>
-      <div className="flex flex-col space-y-6 md:flex-row md:space-x-6 md:space-y-0">
-        <div className="space-y-6 md:w-1/2">
-          {[
-            {
-              heading: `What is Tailwind?`,
-              body: `Tailwind CSS is a highly customizable, low-level CSS framework that gives you all
-              of the building blocks you need to build bespoke designs without any
-              annoying opinionated styles you have to fight to override.`,
-            },
-            {
-              heading: `What is Next.js?`,
-              body: `Next.js is a minimalistic framework for creating server-rendered
-              React applications.`,
-            },
-          ].map((section) => (
-            <div key={section.heading}>
-              <h2 className="mb-3 text-xl font-bold">{section.heading}</h2>
-              <p>{section.body}</p>
-            </div>
-          ))}
-        </div>
-
-        <div className="md:w-1/2">
-          <img
-            alt="A one-eyed alien holding a broken cable connected between a server and a desktop computer"
-            className="w-full"
-            src="critter.svg"
-          />
-        </div>
-      </div>
+      {blogPosts.map((post) => {
+        return (
+          <Link key={post.id} as={`/post/${post.slug}`} href="/post/[slug]">
+            <a className="flex max-w-lg mx-auto mb-16 rounded-lg shadow-lg">
+              <div
+                className="flex-none h-48 overflow-hidden text-center bg-cover rounded-t lg:h-auto lg:w-48 lg:rounded-t-none lg:rounded-l"
+                style={{ backgroundImage: `url(${post.image.url})` }}
+                title={post.title}
+              />
+              <div className="flex flex-col justify-between p-4 leading-normal bg-white rounded-b lg:rounded-b-none lg:rounded-r">
+                <div className="mb-8">
+                  <div className="mb-2 text-xl font-bold text-gray-900">
+                    {post.title}
+                  </div>
+                  <p className="text-base text-gray-700">{post.excerpt}</p>
+                </div>
+                <div className="flex items-center">
+                  <div className="text-sm">
+                    <p className="leading-none text-gray-900">
+                      {post.author.name}
+                    </p>
+                    <p className="text-gray-600">{post.date}</p>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </Link>
+        );
+      })}
     </Layout>
   );
 }
 
 export default Magazine;
 
-Magazine.getInitialProps = () => {
-  return {
-    data: {
-      og: {
-        description: "What Telmo uses on a daily basis.",
-        image: "https://telmo.im/og/uses.png"
-      }
-    }
-  }
-}
+// Magazine.getInitialProps = () => {
+//   return {
+//     data: {
+//       og: {
+//         description: "What Telmo uses on a daily basis.",
+//         image: "https://telmo.im/og/uses.png"
+//       }
+//     }
+//   }
+// }
---
+++ pages/post/[slug].js
@@ -0,0 +1,99 @@
+import { GraphQLClient } from "graphql-request";
+import Link from "next/link";
+
+const graphcms = new GraphQLClient(process.env.GRAPHQL_URL_ENDPOINT);
+
+export async function getStaticProps({ params }) {
+  const { blogPost } = await graphcms.request(
+    `
+    query Post($slug: String!) {
+      blogPost(where: { slug: $slug }) {
+        id
+        title
+        content
+        slug
+        image {
+          id
+          url
+        }
+        author {
+          id
+          name
+        }
+        date
+      }
+    }
+  `,
+    {
+      slug: params.slug,
+    }
+  );
+
+  return {
+    props: {
+      blogPost,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const { blogPosts } = await graphcms.request(`
+    {
+        blogPosts {
+            id
+            title
+            summary
+            slug
+            image {
+                url
+                id
+            }
+            date
+            author {
+                name
+            }
+            }
+    }
+  `);
+
+  return {
+    paths: blogPosts.map(({ slug }) => ({
+      params: { slug },
+    })),
+    fallback: false,
+  };
+}
+
+export default ({ blogPost }) => {
+  return (
+    <div className="min-h-screen py-16 bg-gray-100">
+      <div className="max-w-lg mx-auto mb-16 rounded-lg shadow-lg">
+        <div
+          className="flex-none h-48 overflow-hidden text-center bg-cover rounded-t"
+          style={{ backgroundImage: `url(${blogPost.image.url})` }}
+          title={blogPost.title}
+        />
+        <div className="flex flex-col justify-between p-4 leading-normal bg-white rounded-b lg:rounded-b-none lg:rounded-r">
+          <div className="mb-8">
+            <div className="mb-2 text-xl font-bold text-gray-900">
+              {blogPost.title}
+            </div>
+            {/* <p className="text-base text-gray-700">{blogPost.content}</p> */}
+            <div dangerouslySetInnerHTML={{ __html: blogPost.content }} />
+          </div>
+          <div className="flex items-center">
+            <div className="text-sm">
+              <p className="leading-none text-gray-900">{blogPost.author.name}</p>
+              <p className="text-gray-600">{blogPost.date}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="max-w-lg mx-auto">
+        <Link href="/magazine">
+          <a>Back to all blogPosts</a>
+        </Link>
+      </div>
+    </div>
+  );
+};
