a2671577c11cf26953fe096c25482ec2c2dc6e1b
GT Fari <guideg6@gmail.com>
2020-09-03 10:53:20 +0200

    footer wip

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer className="bg-secondary">
-      <ul className="flex items-center justify-between max-w-4xl p-4 mx-auto text-sm text-white md:p-8">
+      <ul className="flex items-center justify-between max-w-4xl p-4 mx-auto text-sm md:p-8">
         <li>
           Created by{" "}
           <a href="https://goosebumps.co.zw" target="_blank" className="font-bold">
