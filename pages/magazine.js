e1ae84750d66721f53778dca1cce3745f4778173
GT Fari <guideg6@gmail.com>
2020-09-03 10:07:35 +0200

    blog card with no images

--- pages/magazine.js
+++ pages/magazine.js
@@ -43,10 +43,10 @@ function Magazine({ og, blogPosts }) {
     return (
     <Layout>
 
-<section className="text-gray-700 blog body-font">
+<section className=" blog body-font">
       <div className="container px-5 py-24 mx-auto">
         <div className="flex flex-col flex-wrap items-center w-full mb-20 text-center">
-          <h1 className="mb-2 text-2xl font-medium text-gray-900 sm:text-3xl title-font">MAGAZINE</h1>
+          <h1 className="mb-2 text-2xl font-medium sm:text-3xl title-font">MAGAZINE</h1>
           <p className="w-full text-base leading-relaxed lg:w-1/2">
           People thrive when you empower them to shape their own story</p>
         </div>
@@ -57,20 +57,17 @@ function Magazine({ og, blogPosts }) {
             <Link key={post.id} as={`/post/${post.slug}`} href="/post/[slug]">
               <a className="flex flex-col items-center justify-center max-w-sm p-4 mx-auto mb-6 md:w-1/3 md:mb-0 blog-card">
               
-                  <img className="w-full h-56 bg-gray-300 bg-center bg-cover rounded-lg shadow-md image-box" src={post.image.url}
+                  {/* <img className="w-full h-56 bg-gray-300 bg-center bg-cover rounded-lg shadow-md image-box" src={post.image.url}
                       title={post.title}
                       loading="lazy"
-                      />
+                      /> */}
 
-                  <div className="z-10 inline-block p-5 -mt-10 overflow-hidden bg-white rounded-lg shadow-lg text-box">
+                  <div className="z-10 inline-block p-5 -mt-10 overflow-hidden rounded-lg shadow-lg text-background bg-writing text-box">
                     
                     <div className="inline-flex header-content ">
-                      <div className="flex-1 w-4 h-4 m-1 bg-blue-100 rounded-full category-badge m">
-                        <div className="w-2 h-2 m-1 bg-blue-500 rounded-full " ></div>
-                      </div>
                       {post.categories.map((category) => {
                           return (
-                            <span key={category} className='flex items-center justify-center flex-1 h-auto mx-1 text-xs leading-none category-title'>#{category}</span>
+                            <span key={category} className='flex items-center justify-center flex-1 h-auto mx-1 text-xs leading-none text-highlight category-title'>#{category}</span>
                         );
                         })}
                     </div>
@@ -79,8 +76,8 @@ function Magazine({ og, blogPosts }) {
                     <p className="h-20 overflow-y-scroll leading-relaxed md:text-xs lg:text-sm summary-post">{post.summary}</p>
                     <div className='flex flex-row justify-between mt-4'>
                       <span className='flex flex-col'>
-                        <p className="inline-block text-xs text-gray-600 border-b-2 border-blue-500 w-fit-content">{post.author ? post.author.name : `anonymous`}</p>
-                        <p className="text-xs text-gray-500 "><Date dateString={post.date} /></p>
+                        <p className="inline-block text-xs border-b-2 border-primary w-fit-content">{post.author ? post.author.name : `anonymous`}</p>
+                        <p className="text-xs "><Date dateString={post.date} /></p>
                       </span>
                     </div>
                   </div>
