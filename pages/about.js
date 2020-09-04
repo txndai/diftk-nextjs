0e4a6c8e507deacc70082d7fee1f3d52de45689f
GT Fari <guideg6@gmail.com>
2020-09-05 00:05:35 +0200

    team members added & pulled from cms

--- pages/about.js
+++ pages/about.js
@@ -1,51 +1,52 @@
 import Layout from "../components/layout";
+import { GraphQLClient } from "graphql-request";
 
-function AboutPage({ og }) {
+const graphcms = new GraphQLClient(process.env.GRAPHQL_URL_ENDPOINT);
+
+export async function getStaticProps() {
+    const { teams } = await graphcms.request(
+      `
+      query MyQuery() {
+        teams {
+          name
+          picture {
+            url
+          }
+          quote
+          role
+        }
+      }
+    `
+    );
+  
+    return {
+      props: {
+        teams,
+      },
+    };
+}
+
+
+function AboutPage({ teams }) {
+  console.log(teams)
   return (
     <Layout>
-      <div className="flex flex-col space-y-6 md:flex-row md:space-x-6 md:space-y-0">
+      <div className="container flex flex-col items-center justify-center px-5 py-24 mx-auto">
         <div className="space-y-6 md:w-1/2">
-          {[
-            {
-              heading: `What is Tailwind?`,
-              body: `Tailwind CSS is a highly customizable, low-level CSS framework that gives you all
-              of the building blocks you need to build bespoke designs without any
-              annoying opinionated styles you have to fight to override.`,
-            },
-            {
-              heading: `What is Next.js?`,
-              body: `Next.js is a minimalistic framework for creating server-rendered
-              React applications.`,
-            },
-          ].map((section) => (
-            <div key={section.heading}>
-              <h2 className="mb-3 text-xl font-bold">{section.heading}</h2>
-              <p>{section.body}</p>
+          
+          {teams.map((member) => (
+            <div key={member.name}>
+              <h2 className="mb-3 text-xl font-bold">{member.name}</h2>
+              <img className='w-10 h-10 rounded-full' src={member.picture.url} alt=""/>
+              <p>{member.role}</p>
+              <p>{member.quote}</p>
             </div>
           ))}
         </div>
 
-        <div className="md:w-1/2">
-          <img
-            alt="A one-eyed alien holding a broken cable connected between a server and a desktop computer"
-            className="w-full"
-            src="critter.svg"
-          />
-        </div>
       </div>
     </Layout>
   );
 }
 
 export default AboutPage;
-
-AboutPage.getInitialProps = () => {
-  return {
-    data: {
-      og: {
-        description: "What Telmo uses on a daily basis.",
-        image: "https://telmo.im/og/uses.png"
-      }
-    }
-  }
-}
